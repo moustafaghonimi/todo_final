5cbd9595ff279964f42587dd398157db4a0df4a7
mostafa <mustafaghonamy2002@gmail.com>
2022-12-11 13:08:33 +0200

    add structure to app

---
+++ lib/Layout/home_layout.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+static const String routename='home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+
+    );
+  }
+}
